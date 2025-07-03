c04bcf188cc5a9bdc40abecc189ebbf1460c5272
MajorTomAW <tom.wrnr.private@gmail.com>
2025-07-03 17:54:16 +0200

    Botani Mover

---
+++ Source/CommonMover/Private/Library/CommonMovementCheckUtils.cpp
@@ -0,0 +1,29 @@
+﻿// Copyright © 2025 Playton. All Rights Reserved.
+
+
+#include "Library/CommonMovementCheckUtils.h"
+
+#include "MoverSimulationTypes.h"
+
+
+#include UE_INLINE_GENERATED_CPP_BY_NAME(CommonMovementCheckUtils)
+
+bool UCommonMovementCheckUtils::IsFalling(const FMoverSyncState& SyncState)
+{
+	return SyncState.MovementMode == DefaultModeNames::Falling;
+}
+
+bool UCommonMovementCheckUtils::IsWalking(const FMoverSyncState& SyncState)
+{
+	return SyncState.MovementMode == DefaultModeNames::Walking;
+}
+
+bool UCommonMovementCheckUtils::IsFlying(const FMoverSyncState& SyncState)
+{
+	return SyncState.MovementMode == DefaultModeNames::Flying;
+}
+
+bool UCommonMovementCheckUtils::IsSwimming(const FMoverSyncState& SyncState)
+{
+	return SyncState.MovementMode == DefaultModeNames::Swimming;
+}
---
+++ Source/CommonMover/Public/Library/CommonMovementCheckUtils.h
@@ -0,0 +1,30 @@
+﻿// Copyright © 2025 Playton. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "CommonMovementCheckUtils.generated.h"
+
+struct FMoverSyncState;
+/**
+ * 
+ */
+UCLASS()
+class COMMONMOVER_API UCommonMovementCheckUtils : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "Mover|State")
+	static bool IsFalling(const FMoverSyncState& SyncState);
+
+	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "Mover|State")
+	static bool IsWalking(const FMoverSyncState& SyncState);
+
+	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "Mover|State")
+	static bool IsFlying(const FMoverSyncState& SyncState);
+
+	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "Mover|State")
+	static bool IsSwimming(const FMoverSyncState& SyncState);
+};
